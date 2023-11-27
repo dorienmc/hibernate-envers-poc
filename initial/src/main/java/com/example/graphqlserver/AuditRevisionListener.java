ef179363b039b0f02c3f835b014db35ecfe91580
Dorien Lorijn - Meijer Cluwen <dorien.meijercluwen@nedap.com>
2023-11-27 10:32:37 +0100

    Add context info to audit revision

--- initial/src/main/java/com/example/graphqlserver/AuditRevisionListener.java
+++ initial/src/main/java/com/example/graphqlserver/AuditRevisionListener.java
@@ -1,20 +1,31 @@
 package com.example.graphqlserver;
 
 import org.hibernate.envers.RevisionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
 import com.example.graphqlserver.entity.AuditRevisionEntry;
 
 /**
- * TODO: Description
  * @author Dorien Lorijn
  */
 public class AuditRevisionListener implements RevisionListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuditRevisionListener.class);
+
     @Override
     public void newRevision(Object revisionEntity) {
-        String currentUser = "tmp";
+        var requestAttributes = RequestContextHolder.currentRequestAttributes();
+        var headerName = ContextKey.USER_ID.getHeader();
+        var currentUser = requestAttributes.getAttribute(headerName, RequestAttributes.SCOPE_SESSION);
+        if(currentUser == null) {
+            currentUser = "unknown";
+        }
+        logger.info(String.format("Retrieving %s: %s from the context", headerName, currentUser));
 
         AuditRevisionEntry audit = (AuditRevisionEntry) revisionEntity;
-        audit.setUser(currentUser);
+        audit.setUser(currentUser.toString());
     }
 }
